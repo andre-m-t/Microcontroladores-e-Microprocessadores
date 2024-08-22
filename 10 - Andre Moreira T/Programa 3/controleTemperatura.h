c9225331dda399530945cd49680f464e422912f0
André M.  Tavares <148511756+andre-m-t@users.noreply.github.com>
2024-08-22 13:35:54 -0300

    Add files via upload

---
+++ 10 - Andre Moreira T/Programa 3/controleTemperatura.h
@@ -0,0 +1,19 @@
+#include <16F877A.h>
+#device ADC=10
+
+#FUSES PUT                      //Power Up Timer
+#FUSES NOBROWNOUT               //No brownout reset
+#FUSES LVP                      //Low Voltage Programming on B3(PIC16) or B5(PIC18)
+#FUSES NOCPD                    //No EE protection
+#FUSES NOWRT                    //Program memory not write protected
+#FUSES NOPROTECT                //Code not protected from reading
+
+#use delay(crystal=8MHz)
+
+
+#use fixed_io(c_outputs=PIN_C2, PIN_C1)
+
+#define  B_LIGA   PIN_B0
+#define  B_DESL   PIN_B1
+#define  HEATER   PIN_C2
+#define  FAN      PIN_C1
